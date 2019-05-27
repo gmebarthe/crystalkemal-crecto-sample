f0915ca8a46e72bcb2beea04545cd9575ffbf105
gmebarthe <guillaume.barthe@gmail.com>
2019-05-27 08:24:40 +0200

    Update app.cr

--- src/app.cr
+++ src/app.cr
@@ -9,8 +9,8 @@ module Repo
       conf.adapter = Crecto::Adapters::Postgres # or Crecto::Adapters::Mysql or Crecto::Adapters::SQLite3
       conf.database = "kemal"
       conf.hostname = "localhost"
-      conf.username = "gmebarthe"
-      conf.password = ""
+      conf.username = "XXXXX"
+      conf.password = "YYYYY"
       conf.port = 5432
       # you can also set initial_pool_size, max_pool_size, max_idle_pool_size,
       # checkout_timeout, retry_attempts, and retry_delay
